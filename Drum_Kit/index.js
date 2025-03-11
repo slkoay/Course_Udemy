dd8d1303b54b671220decc6973d057869c452311
slkoay <soonleekoay@yahoo.com>
2025-03-11 11:05:36 +0800

    Update index.js

--- Drum_Kit/index.js
+++ Drum_Kit/index.js
@@ -1,6 +1,8 @@
 var all_button = document.querySelectorAll("button");
+var audio = new Audio("./sounds/tom-1.mp3");
 
 for(i=0;i<all_button.length;i++){
-  all_button[i].addEventListener("click", function(){ alert("Cliked");});
+  all_button[i].addEventListener("click", function(){ 
+    audio.play();});
 };
 
