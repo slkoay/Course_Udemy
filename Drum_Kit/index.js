4d1d24bcb532a340acf3154fab55986d5989aca2
slkoay <soonleekoay@yahoo.com>
2025-03-11 10:25:10 +0800

    Update index.js

--- Drum_Kit/index.js
+++ Drum_Kit/index.js
@@ -1,1 +1,6 @@
-alert("It is Working");
+var all_button = document.querySelectorAll("button");
+
+for(i=0;i<all_button.length;i++){
+  all_button[i].addEventListener("click", function(){ alert("Cliked");});
+};
+
