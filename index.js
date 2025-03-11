7fba5cd5f6cb406d2e1b1e98d920b7e4800ad2a8
slkoay <soonleekoay@yahoo.com>
2025-03-11 09:07:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var n2 = Math.floor(Math.random()*6)+1;  //Generate number 1
 var p1 = document.querySelectorAll(".dice .img1");
 var p2 = document.querySelectorAll(".dice .img2");
 
-var roll_1="./dice" + n1 + ".png"; 
-var roll_2="./dice" + n2 + ".png"; 
+//var roll_1="./dice" + n1 + ".png"; 
+//var roll_2="./dice" + n2 + ".png"; 
 
 p1[0].setAttribute("src", "./dice" + n1 + ".png");
-p2[0].setAttribute("src", roll_2);  //"./dice1.png";
+p2[0].setAttribute("src", "./dice" + n2 + ".png");  //"./dice1.png";
 
 if (n1>n2){
   document.querySelector("h1").innerHTML="Player 1 Wins";
