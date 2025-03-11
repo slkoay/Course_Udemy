0fa0903543a5ad36bad0ee411b35dc32b33eddd1
slkoay <soonleekoay@yahoo.com>
2025-03-11 12:11:52 +0800

    Update index.js

--- Drum_Kit/index.js
+++ Drum_Kit/index.js
@@ -4,7 +4,42 @@ var audio = new Audio("./sounds/tom-1.mp3");
 for(i=0;i<all_button.length;i++){
   all_button[i].addEventListener("click", function(){ 
     //audio.play();
-    console.log(this.innerHTML);});
+    //console.log(this.innerHTML);
+
+    switch(this.innerHTML) {
+  case w:
+    var tom1 = new Audio("./sounds/tom-1.mp3");
+    tom1.play();    
+    break;
+  case a:
+    var tom2 = new Audio("./sounds/tom-2.mp3");
+    tom2.play();    
+    break;
+  case s:
+    var tom3 = new Audio("./sounds/tom-3.mp3");
+    tom3.play();    
+    break;
+  case d:
+    var tom4 = new Audio("./sounds/tom-4.mp3");
+    tom4.play();    
+    break;
+  case j:
+    var tom1 = new Audio("./sounds/tom-1.mp3");
+    tom1..play();    
+    break;
+  case k:
+    var tom1 = new Audio("./sounds/tom-1.mp3");
+    tom1..play();    
+    break;
+  case l:
+    var tom1 = new Audio("./sounds/tom-1.mp3");
+    tom1..play();    
+    break;
+  default:
+    // code block
+}
+  
+  });
 
 
 };
