6895fbf3b5141840051bf75dfbf2c0985c5a91dd
slkoay <soonleekoay@yahoo.com>
2025-03-12 16:16:50 +0800

    Update game.js

--- Simon_Game/game.js
+++ Simon_Game/game.js
@@ -9,28 +9,8 @@ function nextSequence(){
    gamePattern.push(randomChosenColour);
    $("#"+randomChosenColour).fadeOut("fast");
    $("#"+randomChosenColour).fadeIn("fast");
-   playSound(randomChosenColour);
+   var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+   audio.play();
 }
 
 
-function playSound(color){
-   switch(color) {
-  case "red":
-    var color1 = new Audio("./sounds/red.mp3");
-    color1.play();    
-    break;
-  case "blue":
-    var color2 = new Audio("./sounds/blue.mp3");
-    color2.play();    
-    break;
-  case "green":
-    var color3 = new Audio("./sounds/green.mp3");
-    color3.play();    
-    break;
-  case "yellow":
-    var color4 = new Audio("./sounds/yellow.mp3");
-    color4.play();    
-    break;
-  default:
-    console.log(keyvalue);
-}};
