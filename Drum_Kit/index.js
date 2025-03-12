d881ee7b09eac001861aab79379807bb11c035ed
slkoay <soonleekoay@yahoo.com>
2025-03-12 09:04:58 +0800

    Update index.js

--- Drum_Kit/index.js
+++ Drum_Kit/index.js
@@ -51,6 +51,6 @@ for(i=0;i<all_button.length;i++){
 
 function btnAnimation(currentKey){
    var activeButton = document.querySelector("."+currentKey);
-   activeButton.classList.toggle("pressed");
-   activeButton.classList.toggle("pressed");
+   activeButton.classList.add("pressed");
+   activeButton.classList.remove("pressed");
 }
