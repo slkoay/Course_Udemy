7a12b960c0925caac9dd65576df3f849f42110db
slkoay <soonleekoay@yahoo.com>
2025-03-13 10:54:35 +0800

    Update game.js

--- Simon_Game/game.js
+++ Simon_Game/game.js
@@ -2,6 +2,8 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern =[];
+var game_status=[];
+var level = 0;
 
 function nextSequence(){
    var randomNumber=Math.floor(Math.random()*4);
@@ -10,6 +12,8 @@ function nextSequence(){
    $("#"+randomChosenColour).fadeOut("fast");
    $("#"+randomChosenColour).fadeIn("fast");
    playSound(randomChosenColour);
+   level+=1;
+   $("h1").text("Level "+level);
 }
 
 $(".btn").click(function(){
@@ -17,8 +21,19 @@ $(".btn").click(function(){
   console.log(userChosenColour);
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
-   animatePress(userChosenColour);
+  animatePress(userChosenColour);
 });
+
+
+$(document).keypress(function(event){
+   if (game_status.length === 0){
+      nextSequence();
+      game_status.push(event.key);
+      console.log(event.key);
+   }
+   
+});
+
 
 
 function playSound(name){
