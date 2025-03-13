fd62285d0de7c95d7a75a83c3a28e2987ed5acfe
slkoay <soonleekoay@yahoo.com>
2025-03-13 14:22:48 +0800

    Update game.js

--- Simon_Game/game.js
+++ Simon_Game/game.js
@@ -38,7 +38,7 @@ function checkAnswer(currentLevel){
   console.log("userClickedPattern " + userClickedPattern);
     console.log("gamePattern " + gamePattern);
   console.log(gamePattern[currentLevel-1]);
-  if (gamePattern[currentLevel-1]===userClickedPattern){
+  if (gamePattern[currentLevel-1]==userClickedPattern){
     console.log("correct");
     if(userClickedPattern.length===level){
       userClickedPattern =[];
