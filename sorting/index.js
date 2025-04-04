8328c8ed27920a019d15406368bef1d1e2d3a876
slkoay <soonleekoay@yahoo.com>
2025-04-04 15:37:56 +0800

    Update index.js

--- sorting/index.js
+++ sorting/index.js
@@ -1,1 +1,8 @@
+var data = [{"id":1, "UserName":"Doe", "UserOccupation":"TestOccu"}];
+
+
+$(".btn").click(function(){
+  data.push({"id":data.length+1 ,"UserName":"test", "UserOccupation":"Test2"});
+  
+});
 
