ba6f054b6712c2831ec45e9348b01ab07afad1d2
slkoay <soonleekoay@yahoo.com>
2025-04-04 16:19:34 +0800

    Update index.js

--- sorting/index.js
+++ sorting/index.js
@@ -1,10 +1,10 @@
 var data = [{"id":1, "UserName":"Doe", "UserOccupation":"TestOccu"}];
+let h = "";
 
 
-$(".btn").click(function(){
-  data.push({"id":data.length+1 ,"UserName":"test", "UserOccupation":"Test2"});
-  
-});
+for (i=0;i<data.length;i++){
+   h+="<tr><td>" + data[i]["id"] + "</td><td>" + data[i]["UserName"] + "</td><td>" + data[i]["UserOccupation"] +"</td></tr>";
+    
+}
 
-document.getElementById("demo").innerHTML = "The value of z is: " + data;
-
+document.getElementById("demo").innerHTML = "<table><tr><th>ID</th><th>UserName</th><th>UserOccupation</th></tr>" + h + "</table>";
